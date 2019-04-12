8a7d9bf3d3e595c3873ac01bf8a3256886429483
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2019-04-12 15:46:30 -0400

    refactor: Change the code to backup object it not destroyed
    The isse was that in every iteration it created a different
    object with different data. If it want to make a multithreaded
    program it must to fix that.
    
    Resolves: iss3117

--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,7 @@
 int main()
 {
     menu menuGen;
-    char answer;
-
-    do
-    {
-        menuGen.listItems();
-        menuGen.handleItems();
-
-        std::cout << "\nDo you want to exit? (y/n): ";
-        std::cin >> answer;
-        std::cout << "\n";
-    }while(answer == 'n' || answer == 'N');
+    menuGen.loop();
 
 	return 0;
 }

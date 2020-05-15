5a3e05698f0484d3051f14e3ab35efde9c26a850
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2020-05-15 07:11:49 -0400

    style: Arrange the source idents
    Idents of scripts.

--- src/prepareDB.sh
+++ src/prepareDB.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Variables
-    userDB="jfrc26"
-    passwordDB="-2160JfRc*"
-    database="jfrc26"
-    output=`date +%Y-%m-%d_%H-%M-%S`_$database.sql
-    
+userDB="jfrc26"
+passwordDB="-2160JfRc*"
+database="jfrc26"
+output=`date +%Y-%m-%d_%H-%M-%S`_$database.sql
+
 # Database output
-    mysqldump --user=$user --password=$password $database > $output
-    
+mysqldump --user=$user --password=$password $database > $output
+
 # Compressing
-    gzip $output
+gzip $output

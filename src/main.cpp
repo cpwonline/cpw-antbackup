c2f39813a93a6c0eadde12e14bb71c5233d4240e
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2020-05-29 15:29:35 -0400

    test: Try to use pointers

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,17 @@
 /*
  * main.cc
  * Copyright (C) 2019 CPW Online <cpwonline@otmail.com>
- * 
+ *
  * net.cpwonline.cpw-antbackup is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * net.cpwonline.cpw-antbackup is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License along
  * with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -30,7 +30,7 @@ int main()
 	backups *bakGen = new backups;
 	bakGen->hola();
 	delete bakGen;
-	
+
 	return 0;
 }
 

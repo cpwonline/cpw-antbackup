bf790225bf3b2fae1db872aae7a0a1a5077d7634
Jose Felix Rivas Carrasco <josefelixrc@outlook.com>
2020-08-15 11:45:20 -0400

    test: Test the files preparation

--- prepareFiles.sh
+++ prepareFiles.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-if [ -n "$1" ]; then
+if [ -n "$1" ; -n "$2" ; -n "$3"]; then
 
 	# Variables
 		title=$1
-		files=$2
+		files="$2 $3"
 		output=`date +%Y-%m-%d_%H-%M-%S`_${title}.tar
 
 	# Packing files
 		tar -cv $files -f $output
 
 	# Compressing
-		gzip $output
+		tar -Jcv $output -f "$output".xz
 
+	# Delete
+		rm -rf $output
 else
 
 	echo "No parameters found. "
